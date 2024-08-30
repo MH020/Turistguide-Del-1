62e2209a6437bd38466f95fc54bf791a16093bb9
MH020 <166133291+MH020@users.noreply.github.com>
2024-08-30 15:21:02 +0200

    added project

---
+++ Turistguide Del 1/src/main/java/org/example/turistguide_del_1/TuristguideDel1Application.java
@@ -0,0 +1,13 @@
+package org.example.turistguide_del_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristguideDel1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristguideDel1Application.class, args);
+    }
+
+}
---
+++ Turistguide Del 1/src/test/java/org/example/turistguide_del_1/TuristguideDel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turistguide_del_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristguideDel1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
