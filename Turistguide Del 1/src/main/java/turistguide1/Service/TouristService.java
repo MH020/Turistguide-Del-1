9fd3690583bbe5d33342e6dd70d631271375a95c
MH020 <166133291+MH020@users.noreply.github.com>
2024-09-01 15:54:08 +0200

    prøver at lave service nu

--- Turistguide Del 1/src/main/java/turistguide1/Service/TouristService.java
+++ Turistguide Del 1/src/main/java/turistguide1/Service/TouristService.java
@@ -1,4 +1,18 @@
 package turistguide1.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import turistguide1.Model.TouristAttraction;
+import turistguide1.repository.TouristRepository;
+
+@Service
 public class TouristService {
+    // This class is a service class that is used to be the middle man between the controller and the database(Repository).
+    // so i will now implement the CRUD operations in this class that use the repository to do the operations
+
+
+    //create. add a tourist attraction to the list
+    public void addTouristAttraction(TouristAttraction Attraction){
+        TouristRepository.addTouristAttraction(Attraction);
+    }
 }
