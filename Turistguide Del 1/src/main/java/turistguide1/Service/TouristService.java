57cf7459d1afbd9b9ce261f719ae312bee341759
MH020 <166133291+MH020@users.noreply.github.com>
2024-09-01 16:08:56 +0200

    I think service works now it will at least take do crud now that i have addad a constructor that injects the prepository

--- Turistguide Del 1/src/main/java/turistguide1/Service/TouristService.java
+++ Turistguide Del 1/src/main/java/turistguide1/Service/TouristService.java
@@ -1,6 +1,5 @@
 package turistguide1.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import turistguide1.Model.TouristAttraction;
 import turistguide1.repository.TouristRepository;
@@ -8,11 +7,17 @@ import turistguide1.repository.TouristRepository;
 @Service
 public class TouristService {
     // This class is a service class that is used to be the middle man between the controller and the database(Repository).
-    // so i will now implement the CRUD operations in this class that use the repository to do the operations
 
+    private final TouristRepository repository;
+
+    //constructor to inject the repository into the service class so it can be used to do the CRUD operations on the database(Repository)
+    public TouristService(TouristRepository repository) {
+        this.repository = repository;
+    }
+    // so i will now implement the CRUD operations in this class that use the repository to do the operations
 
     //create. add a tourist attraction to the list
     public void addTouristAttraction(TouristAttraction Attraction){
-        TouristRepository.addTouristAttraction(Attraction);
+        repository.addTouristAttraction(Attraction);
     }
 }
--- Turistguide Del 1/src/main/java/turistguide1/Service/TuristguideDel1Application.java
+++ Turistguide Del 1/src/main/java/turistguide1/Service/TuristguideDel1Application.java
@@ -2,8 +2,10 @@ package turistguide1.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "turistguide1")
 public class TuristguideDel1Application {
 
     public static void main(String[] args) {
